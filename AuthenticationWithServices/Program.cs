95cdfd7713fc908e8af1cbd046271d98e4bcb0c6
smamedov451@gmail.com <smamedov451@gmail.com>
2023-07-28 11:13:13 +0300

    Добавьте файлы проекта.

---
+++ AuthenticationWithServices/Program.cs
@@ -0,0 +1,55 @@
+using AuthenticationWithServices.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationWithServices
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer("Server=.\\SQLEXPRESS;Database=TestAuthenticationWithServicesDB;Trusted_Connection=True;TrustServerCertificate=True"));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddAuthentication().AddGoogle(opt => {
+                opt.ClientId = "228416462843-r2d5mijpbbg1alrf542haap41f8tunta.apps.googleusercontent.com";
+                opt.ClientSecret = "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";
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
